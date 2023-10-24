8100da5fd3cdd96d71d2911b11749c254cd26689
irakli379 <123487553+irakli379@users.noreply.github.com>
2023-10-24 15:27:16 +0400

    Update compositions_6.py
    add code

--- compositions_6.py
+++ compositions_6.py
@@ -1,1 +1,48 @@
-state
+class Person:
+    def __init__(self):
+        self.heart = None
+        self.brain = None
+
+
+
+class Heart:
+    def __init__(self, usage):
+        self.usage = usage
+
+    
+    def state(self):
+        if self.usage > 70:
+            return 'high blood pressure'
+        else:
+            return 'feeling good'
+
+
+class Brain:
+    def __init__(self, usage):
+        self.usage = usage
+
+
+    def state(self):
+        if self.usage > 90:
+            return 'tired'
+        else:
+            return 'rested'
+
+
+class Leg:
+    def __init__(self, moving_speed, state='k'):
+        self.moving_speed = moving_speed
+        self.state = state
+
+    @property
+    def state(self):
+        return self._state
+
+    @state.setter
+    def state(self, val):
+        if self.moving_speed > 10:
+            self._state = 'running'
+        else:
+            self._state = 'walking'
+            
+  
